e2cffe9cd15cfbdef293208f9a11f2135b1c8e2f
rishab123-creator <rishabjain13112005123@gmail.com>
2026-04-24 15:05:50 +0530

    fixed bug 6 same as 4 .count is not used .qty is used

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Cart({ items, onRemove, onClose, onUpdateQty }) {
-  const total = items.reduce((sum, item) => sum + item.price * item.count, 0);
+  const total = items.reduce((sum, item) => sum + item.price * item.qty, 0);
 
   return (
     <>
